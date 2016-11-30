7491f8bba2aed903f63e9e395bcbc7b4ccfd4cfe
Ray <hu.ray@foxmail.com>
2016-11-30 22:27:47 +0800

    complete chapter 2

---
+++ easyDemo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+Manager = Manager(app)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello world!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+if __name__ == '__main__':
+    Manager.run()
