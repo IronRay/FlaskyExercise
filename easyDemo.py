7bc6aad977fd2162746ffcd2162e0e47b7ed7707
Ray <hu.ray@foxmail.com>
2016-12-13 17:20:17 +0800

    Update database scripts

--- easyDemo.py
+++ easyDemo.py
@@ -1,22 +1,59 @@
 #!/usr/bin/env python3
 
+import os
+
 from datetime import datetime
 
 from flask import Flask, render_template, url_for, redirect, session, flash
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_wtf import Form
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guss string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    """docstring for Role"""
+
+    # initlize table name
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    """docstring for User"""
+
+    # initlize table name
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
@@ -24,19 +61,33 @@ class NameForm(Form):
     Submit = SubmitField('Submit')
 
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
 
     if form.validate_on_submit():
-        old_name = session.get('name')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
 
-        if (old_name is not None) and (old_name != form.name.data):
-            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
-        return redirect(url_for('user', name=session['name']))
 
-    return render_template('index.html', form=form, name=session.get('name'))
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
